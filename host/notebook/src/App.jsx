c38d9b839f549a693b7bdd376f285a404f354353
Panos Oikonomou <panos.economou.v@gmail.com>
2024-08-09 14:57:50 +0300

    React

---
+++ host/notebook/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Manifold from './assets/Manifold'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hi</h1>
+      <div className="card">
+      <Manifold/>
+      </div>
+    </>
+  )
+}
+
+export default App
